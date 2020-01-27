0b0f3e68d5f1f567f67a735a4784ef2e7655b6dd
mariamiah <mariah.kakande@gmail.com>
2020-01-28 00:22:53 +0300

    set up models

---
+++ wagtail_blog/blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+import wagtail
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+# Create your models here.
+class BlogPage(Page):
+    description = models.CharField(max_length=255, blank=True,)
+    content_panels = Page.content_panels + [
+        FieldPanel('description', classname="full")
+    ]
+
+class PostPage(Page):
+    body = RichTextField(blank=True)
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full")
+    ]
